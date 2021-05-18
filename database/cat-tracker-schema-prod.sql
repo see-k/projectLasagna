55bdf3c0bd831626041560b1a93ddc78d5586ca3
see-k <flabioski@gmail.com>
2021-05-18 15:50:19 -0500

    database directories & comments

---
+++ database/cat-tracker-schema-prod.sql
@@ -0,0 +1,21 @@
+-- Cats production script
+-- Author Chike Okonta, Quinn Chu, Derrick Fidleman
+-- Date 05/20/2021
+
+drop database if exists cat_tracker;
+create database cat_tracker;
+use cat_tracker;
+
+-- User: assigned to Chike Okonta
+
+-- Location: assigned to Chike Okonta
+
+-- Sightings: assigned to Quinn Chu
+
+-- Cats: assigned to Quinn Chu
+
+-- Alias: assigned to Derrick Fidleman
+
+-- Reports: assigned to Derrick Fidleman
+
+-- create tables and relationships
---
+++ database/cat-tracker-schema-test.sql
@@ -0,0 +1,23 @@
+-- Cats test script
+-- Author Chike Okonta, Quinn Chu, Derrick Fidleman
+-- Date 05/20/2021
+
+drop database if exists cat_tracker_test;
+create database cat_tracker_test;
+use cat_tracker_test;
+
+-- Set known good state: assigned to Chike Okonta, Quinn Chu, Derrick Fidleman
+
+-- User: assigned to Chike Okonta
+
+-- Location: assigned to Chike Okonta
+
+-- Sightings: assigned to Quinn Chu
+
+-- Cats: assigned to Quinn Chu
+
+-- Alias: assigned to Derrick Fidleman
+
+-- Reports: assigned to Derrick Fidleman
+
+-- create tables and relationships
