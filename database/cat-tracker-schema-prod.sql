12fe152be3f0f1ce8b0f49fbf1596c27a95de9ad
qc2xt <qc2xt@virginia.edu>
2021-05-19 09:24:12 -0500

    updates

--- database/cat-tracker-schema-prod.sql
+++ database/cat-tracker-schema-prod.sql
@@ -14,36 +14,6 @@ use cat_tracker;
 
 -- Alias: assigned to Derrick Fidleman
 
--- Sightings: assigned to Quinn Chu
-create table sighting (
-	sighting_id int primary key not null auto_increment,
-    img_path varchar(100),
-    sighting_description varchar(300),
-    disabled bit not null default 0,
-    constraint fk_sighting_users_id
-		foreign key (users_id)
-        references users(users_id),
-	constraint fk_sighting_location_id
-		foreign key (location_id)
-        references location(location_id),
-	constraint fk_sighting_cat_id
-		foreign key (cat_id)
-        references cat(cat_id)
-);
--- Reports: assigned to Quinn Chu
-create table report (
-	report_id int not null auto_increment,
-    description varchar(300),
-    constraint fk_report_cat_id
-		foreign key (cat_id)
-        references cat(cat_id),
-	constraint fk_report_users_id
-		foreign key (users_id)
-        references users(users_id),
-    constraint fk_report_sighting_id
-		foreign key (sighting_id)
-        references sighting(sighting_id)
-);
 
 
 -- create tables and relationships
