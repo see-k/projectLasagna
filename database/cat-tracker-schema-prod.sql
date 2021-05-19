6e767d2313e8f63e739a7f69bc2c7afcafbb598a
qc2xt <qc2xt@virginia.edu>
2021-05-19 09:28:46 -0500

    ajskndkajsd

--- database/cat-tracker-schema-prod.sql
+++ database/cat-tracker-schema-prod.sql
@@ -1,3 +1,4 @@
+  
 -- Cats production script
 -- Author Chike Okonta, Quinn Chu, Derrick Fidleman
 -- Date 05/20/2021
@@ -6,8 +7,79 @@ drop database if exists cat_tracker;
 create database cat_tracker;
 use cat_tracker;
 
+-- User: assigned to Chike Okonta
+CREATE TABLE users (
+	users_id int PRIMARY KEY AUTO_INCREMENT,
+    users_name VARCHAR(25) NOT NULL,
+    users_email VARCHAR(50),
+    diabled BOOLEAN NOT NULL DEFAULT(0)
+);
 
+-- Location: assigned to Chike Okonta
+CREATE TABLE location (
+	location_id int PRIMARY KEY AUTO_INCREMENT,
+    latitude DECIMAL(8,6),
+    longitude DECIMAL(8,6)
+);
 
+-- Cat: assigned to Derrick Fidelman
+CREATE TABLE cat (
+	cat_id int PRIMARY KEY AUTO_INCREMENT,
+    cat_name varchar(50) NOT NULL,
+    img_path varchar(100) NULL,
+    cat_description varchar(300) NULL,
+    disabled bit NOT NULL,
+    users_id int NOT NULL,
+    CONSTRAINT fk_cat_users_id
+		FOREIGN KEY (users_id)
+        REFERENCES users(users_id)
+);
 
+-- Alias: assigned to Derrick Fidelman
+CREATE TABLE alias (
+	alias_id int PRIMARY KEY AUTO_INCREMENT,
+    alias_name varchar(50) NOT NULL,
+    cat_id int NOT NULL,
+    CONSTRAINT fk_alias_cat_id
+		FOREIGN KEY (cat_id)
+        REFERENCES cat(cat_id)
+);
+
+-- Sightings: assigned to Quinn Chu
+create table sighting (
+	sighting_id int primary key not null auto_increment,
+    img_path varchar(100),
+    sighting_description varchar(300),
+    disabled bit not null default 0,
+    users_id int NOT NULL,
+    location_id int NOT NULL,
+    cat_id int,
+    constraint fk_sighting_users_id
+		foreign key (users_id)
+        references users(users_id),
+	constraint fk_sighting_location_id
+		foreign key (location_id)
+        references location(location_id),
+	constraint fk_sighting_cat_id
+		foreign key (cat_id)
+        references cat(cat_id)
+);
+-- Reports: assigned to Quinn Chu
+create table report (
+	report_id int PRIMARY KEY AUTO_INCREMENT,
+    report_description varchar(300),
+    cat_id int,
+    users_id int,
+    sighting_id int,
+    constraint fk_report_cat_id
+		foreign key (cat_id)
+        references cat(cat_id),
+	constraint fk_report_users_id
+		foreign key (users_id)
+        references users(users_id),
+    constraint fk_report_sighting_id
+		foreign key (sighting_id)
+        references sighting(sighting_id)
+);
 
 -- create tables and relationships
--- database/cat-tracker-schema-test.sql
+++ database/cat-tracker-schema-test.sql
@@ -6,6 +6,37 @@ drop database if exists cat_tracker_test;
 create database cat_tracker_test;
 use cat_tracker_test;
 
+-- Set known good state: assigned to Chike Okonta, Quinn Chu, Derrick Fidleman
 
+-- User: assigned to Chike Okonta
+
+-- Location: assigned to Chike Okonta
+
+-- Alias: assigned to Derrick Fidelman
+CREATE TABLE alias (
+	alias_id int PRIMARY KEY AUTO_INCREMENT,
+    alias_name varchar(50) NOT NULL,
+    cat_id int NOT NULL,
+    CONSTRAINT fk_alias_cat_id
+		FOREIGN KEY (cat_id)
+        REFERENCES cat(cat_id)
+);
+
+-- Cat: assigned to Derrick Fidelman
+CREATE TABLE cat (
+	cat_id int PRIMARY KEY AUTO_INCREMENT,
+    cat_name varchar(50) NOT NULL,
+    img_path varchar(100) NULL,
+    cat_description varchar(300) NULL,
+    disabled bit NOT NULL,
+    user_id int NOT NULL,
+    CONSTRAINT fk_cat_user_id
+		FOREIGN KEY (user_id)
+        REFERENCES `user`(user_id)
+);
+
+-- Sighting: assigned to Quinn Chu
+
+-- Report: assigned to Quinn Chu
 
 -- create tables and relationships
