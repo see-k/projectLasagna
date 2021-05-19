67daa17c319d25fab52580ffbb0ebc5f9040585b
Derrick Fidelman <dfidelman@dev-10.com>
2021-05-19 14:19:20 -0500

    implemented Cat and Alias model

--- cat-tracker/src/main/java/learn/cat/models/Cat.java
+++ cat-tracker/src/main/java/learn/cat/models/Cat.java
@@ -1,4 +1,71 @@
 package learn.cat.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cat {
+
+    private int catId;
+    private String name;
+    private String desc;
+    private String picture;
+    private User user;
+    private boolean isDisabled;
+    private List<Alias> aliases = new ArrayList<>();
+
+    public int getCatId() {
+        return catId;
+    }
+
+    public void setCatId(int catId) {
+        this.catId = catId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public boolean isDisabled() {
+        return isDisabled;
+    }
+
+    public void setDisabled(boolean disabled) {
+        isDisabled = disabled;
+    }
+
+    public List<Alias> getAliases() {
+        return new ArrayList<>(aliases);
+    }
+
+    public void setAliases(List<Alias> aliases) {
+        this.aliases = aliases;
+    }
 }
