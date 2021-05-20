d1bc985bc8eb6a639f2662415d74908445e0be3c
Quinn Chu <qc2xt@virginia.edu>
2021-05-19 20:56:52 -0500

    sighting and report jdbctemplate repo classes done, EDITS NEEDED for pictures field

--- cat-tracker/src/main/java/learn/cat/data/ReportJdbcTemplateRepository.java
+++ cat-tracker/src/main/java/learn/cat/data/ReportJdbcTemplateRepository.java
@@ -1,4 +1,107 @@
 package learn.cat.data;
 
-public class ReportJdbcTemplateRepository {
+import learn.cat.data.mappers.ReportMapper;
+import learn.cat.data.mappers.SightingMapper;
+import learn.cat.models.Report;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReportJdbcTemplateRepository implements ReportRepository {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public ReportJdbcTemplateRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Report> findAll() {
+        final String sql = "select report_id, report_description, cat_id, users_id, sighting_id "
+                + "from report limit 1000;";
+        return jdbcTemplate.query(sql, new ReportMapper());
+    }
+
+    @Override
+    public Report findById(int reportId) {
+        final String sql = "select report_id, report_description, cat_id, users_id, sighting_id "
+                + "from report "
+                + "where report_id = ?;";
+
+        return jdbcTemplate.query(sql, new ReportMapper(), reportId)
+                .stream()
+                .findFirst().orElse(null);
+    }
+
+    @Override
+    public List<Report> findByUserId(int userId) {
+        final String sql = "select report_id, report_description, cat_id, users_id, sighting_id "
+                + "from report "
+                + "where user_id = ?;";
+        return new ArrayList<>(jdbcTemplate.query(sql, new ReportMapper(), userId));
+    }
+
+    @Override
+    public List<Report> findByCatId(int catId) {
+        final String sql = "select report_id, report_description, cat_id, users_id, sighting_id "
+                + "from report "
+                + "where cat_id = ?;";
+        return new ArrayList<>(jdbcTemplate.query(sql, new ReportMapper(), catId));
+    }
+
+    @Override
+    public Report add(Report report) {
+        final String sql = "insert into report (report_description, cat_id, users_id, sighting_id) "
+                + "values (?,?,?,?);";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        int rowsAffected = jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, report.getReportDescription());
+            ps.setInt(2, report.getCatId());
+            ps.setInt(3, report.getUsersId());
+            ps.setInt(4, report.getReportId());
+            return ps;
+        }, keyHolder);
+
+        if (rowsAffected <= 0) {
+            return null;
+        }
+
+        report.setReportId(keyHolder.getKey().intValue());
+        return report;
+    }
+
+    @Override
+    public boolean update(Report report) {
+        final String sql = "update report set "
+                + "report_description = ?, "
+                + "cat_id = ?, "
+                + "users_id = ?, "
+                + "sighting_id = ? "
+                + "where report_id = ?;";
+        return jdbcTemplate.update(sql, report.getReportDescription(), report.getCatId(), report.getUsersId(), report.getSightingId(), report.getReportId()) > 0;
+    }
+
+    @Override
+    public boolean deleteById(int reportId) {
+        return jdbcTemplate.update(
+                "delete from report where report_id = ?;", reportId) > 0;
+    }
+
+    /*KeyHolder keyHolder = new GeneratedKeyHolder();
+        int rowsAffected = jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            return ps;
+        }, keyHolder);
+
+        if (rowsAffected <= 0) {
+            return null;
+        }*/
 }
--- cat-tracker/src/main/java/learn/cat/data/SightingJdbcTemplateRepository.java
+++ cat-tracker/src/main/java/learn/cat/data/SightingJdbcTemplateRepository.java
@@ -5,8 +5,14 @@ import learn.cat.models.Cat;
 import learn.cat.models.Sighting;
 import learn.cat.models.Users;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SightingJdbcTemplateRepository implements SightingRepository {
 
@@ -25,33 +31,70 @@ public class SightingJdbcTemplateRepository implements SightingRepository {
 
     @Override
     public Sighting findById(int sightingId) {
-        final String sql =
-        return null;
+        final String sql = "select sighting_id, img_path, visual_description, sighting_description, sighting_date, sighting_time, disabled, users_id, location_id, cat_id "
+                + "from sighting "
+                + "where sighting_id = ?;";
+
+        return jdbcTemplate.query(sql, new SightingMapper(), sightingId)
+                .stream()
+                .findFirst().orElse(null);
     }
 
     @Override
     public List<Sighting> findByUser(Users users) {
-        final String sql =
-        return null;
+        final String sql = "select sighting_id, img_path, visual_description, sighting_description, sighting_date, sighting_time, disabled, users_id, location_id, cat_id "
+                + "from sighting "
+                + "where user_id = ?;";
+
+        return new ArrayList<>(jdbcTemplate.query(sql, new SightingMapper(), users.getUserId()));
     }
 
     @Override
     public List<Sighting> findByCat(Cat cat) {
-        final String sql =
-        return null;
+        final String sql = "select sighting_id, img_path, visual_description, sighting_description, sighting_date, sighting_time, disabled, users_id, location_id, cat_id "
+                + "from sighting "
+                + "where cat_id = ?;";
+
+        return new ArrayList<>(jdbcTemplate.query(sql, new SightingMapper(), cat.getCatId()));
     }
 
     @Override
     public Sighting add(Sighting sighting) {
-        final String sql =
-        return null;
+        final String sql = "insert into sighting (img_path, visual_description, sighting_description, sighting_date, sighting_time, disabled, users_id, location_id, cat_id) "
+            + "values (?,?,?,?,?,?,?,?,?);";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        int rowsAffected = jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(2, sighting.getCatDescription());
+            //need to fix pictures first
+            return ps;
+        }, keyHolder);
+
+        if (rowsAffected <= 0) {
+            return null;
+        }
+
+        sighting.setSightingId(keyHolder.getKey().intValue());
+        return sighting;
     }
 
     @Override
     public boolean update(Sighting sighting) {
         final String sql = "update sighting set "
-                + ""
+                + "img_path = ?, "
+                + "visual_description = ?, "
+                + "sighting_description = ?, "
+                + "sighting_date = ?, "
+                + "sighting_time = ?, "
+                + "disabled = ?, "
+                + "users_id = ?, "
+                + "location_id = ?,"
+                + "cat_id = ? "
+                + "where sighting_id = ?;";
+
         return false;
+        //return jdbcTemplate.update(/*update with picture stuff :')))*/);
     }
 
     @Override
