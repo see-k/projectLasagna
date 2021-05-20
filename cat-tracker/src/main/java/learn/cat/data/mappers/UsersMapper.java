5a4821e922ba1f01596fce22f4a1575971279070
see-k <flabioski@gmail.com>
2021-05-20 12:08:37 -0500

    Please work

--- cat-tracker/src/main/java/learn/cat/data/mappers/UsersMapper.java
+++ cat-tracker/src/main/java/learn/cat/data/mappers/UsersMapper.java
@@ -1,4 +1,20 @@
 package learn.cat.data.mappers;
+import learn.cat.models.Users;
+import org.springframework.jdbc.core.RowMapper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-public class UsersMapper {
+public class UsersMapper implements  RowMapper<Users> {
+    @Override
+    public Users mapRow(ResultSet resultSet, int i) throws SQLException{
+        Users users = new Users();
+        users.setUserId(resultSet.getInt("user_id"));
+        users.setUsername(resultSet.getString("username"));
+        users.setFirstName(resultSet.getString("first_name"));
+        users.setLastName(resultSet.getString("last_name"));
+        users.setEmail(resultSet.getString("users_email"));
+        users.setDisabled(resultSet.getBoolean("disabled"));
+        return users;
+    }
+
 }
