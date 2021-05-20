8a0934e40379f4049090d0c4d07868789144aa28
see-k <flabioski@gmail.com>
2021-05-20 13:30:30 -0500

    Update

--- cat-tracker/src/main/java/learn/cat/data/LocationJdbcTemplateRepository.java
+++ cat-tracker/src/main/java/learn/cat/data/LocationJdbcTemplateRepository.java
@@ -1,4 +1,35 @@
 package learn.cat.data;
 
-public class LocationJdbcTemplateRepository {
+import learn.cat.models.Location;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class LocationJdbcTemplateRepository implements LocationRepository {
+
+    @Override
+    public Location findById(int locationId) {
+        return null;
+    }
+
+    @Override
+    public List<Location> findAllByCat(int catId) {
+        return null;
+    }
+
+    @Override
+    public Location add(Location location) {
+        return null;
+    }
+
+    @Override
+    public boolean update(Location location) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteById(int locationId) {
+        return false;
+    }
 }
