b91441ddb26a426919082b2b057ec0b0dbf6877a
Quinn Chu <qc2xt@virginia.edu>
2021-05-21 19:42:38 -0500

    error response class

--- cat-tracker/src/main/java/learn/cat/controllers/SightingController.java
+++ cat-tracker/src/main/java/learn/cat/controllers/SightingController.java
@@ -1,16 +1,44 @@
 package learn.cat.controllers;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import learn.cat.domain.Result;
+import learn.cat.domain.SightingService;
+import learn.cat.models.Sighting;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = {"*"})
 @RequestMapping("/api/sighting")
 public class SightingController {
+
+    private final SightingService service;
+
+    public SightingController(SightingService service) { this.service = service; }
+
+    @GetMapping
+    public List<Sighting> findAll() { return service.findAll(); }
+
+    @GetMapping("/{sightingId}")
+    public Sighting findById(int sightingId) { return service.findById(sightingId); }
+
+    @PostMapping
+    public ResponseEntity<Object> add(@RequestBody @Valid Sighting sighting, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<>(bindingResult.getAllErrors(), HttpStatus.BAD_REQUEST);
+        }
+
+        Result<Sighting> result = service.add(sighting);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
+        }
+        return ErrorResponse.build(result);
+    }
     //(@RequestBody @Valid Sighting sighting BindingResult bindingResult)
     //if (bindingResult.hasErrors()) {
     // return new ResponseEntity<>(bindingResult.getAllErrors(), HttpStatus.BAD_REQUEST()); }
