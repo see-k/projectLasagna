51d42a81a0cd7f6bf83e554d833aba747321845d
Quinn Chu <qc2xt@virginia.edu>
2021-05-23 13:21:39 -0500

    nav bar updates

--- cat-tracker-react/src/App.js
+++ cat-tracker-react/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Nav from './components/Nav';
+import About from './static/About';
+import Contact from './static/Contact'
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Nav />
     </div>
   );
 }
